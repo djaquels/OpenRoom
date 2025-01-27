69ae68cd78c2bb64732f5999853c7c71b95ea529
Hector Jacales <hjacales@gmail.com>
2025-01-27 14:21:39 +0100

    chore: add roomUsers relantionships

--- data/run.sh
+++ data/run.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+
+# Build the Docker image
 docker build -t apidb .
+
+# Check if a container named "postgres-apidb" exists
+if docker ps -a --format '{{.Names}}' | grep -q '^postgres-apidb$'; then
+  echo "Container 'postgres-apidb' already exists. Removing it..."
+  docker rm -f postgres-apidb
+fi
+
+# Run the new container
+echo "Starting new container 'postgres-apidb'..."
 docker run --name postgres-apidb -p 5432:5432 -d apidb
-# for testing
+
+# For testing
+echo "Container 'postgres-apidb' is running."
+docker ps -a | grep postgres-apidb
--- src/main/kotlin/com/djaquels/OpenRoom/controllers/UsersController.kt
+++ src/main/kotlin/com/djaquels/OpenRoom/controllers/UsersController.kt
@@ -1,18 +1,24 @@
 package com.djaquels.OpenRoom.controllers
 import com.djaquels.OpenRoom.models.User
+import com.djaquels.OpenRoom.services.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import com.djaquels.OpenRoom.repositories.UsersRepository
 import org.springframework.stereotype.Controller
 
 @RestController
 @RequestMapping("/api/users")
-class UsersController(@Autowired private val usersRepository: UsersRepository) {
+class UsersController(@Autowired private val userService: UserService) {
     @GetMapping("")
     fun getUsers(): List<User> {
-        return usersRepository.findAll().toList()
+        return userService.getAll()
+    }
+
+    @PostMapping("/create")
+    fun postUser(@RequestBody user: User): ResponseEntity<User> {
+        val newUser = userService.createUser(user)
+        return ResponseEntity(newUser, HttpStatus.CREATED)
     }
 
 }
